00292ee50ade06dab14a2c92f31e0636bbc51b8f
XTOTHEL <xiaoboliu@Webs-MacBook-Pro.local>
2015-02-25 15:24:02 -0500

    Creation of templates and demo home page

---
+++ kit/page.php
@@ -0,0 +1,21 @@
+<?php require 'blocks.inc'; ?>
+<!doctype html>
+<html lang="en">
+    <head>
+		<?php insert_raw('head'); ?>
+
+		<title>COSECO Insurance Company</title>
+
+		<meta name="description" content="">
+        <meta name="keywords" content="">  
+    </head>
+    <body>
+		<form>
+			<?php insert_raw('header'); ?>
+					          
+			<?php require '/sublayouts/index.sub.inc'; ?>     
+
+			<?php insert_raw('footer'); ?>
+	  </form>	
+	</body>
+</html>
